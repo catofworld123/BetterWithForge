3dc6881046471853e33a19c829c22a9abe40819f
cat_of_world <Karacharov123@yandex.ru>
2025-07-28 17:30:49 +0500

    test №2

--- src/main/java/mods/bwf/BWFRegistry.java
+++ src/main/java/mods/bwf/BWFRegistry.java
@@ -7,7 +7,6 @@ public class BWFRegistry {
 
     public static void init() {}
 
-
     public static Item registerItem(String id, Item item) {
         item.setUnlocalizedName("bwp:" + id);
         item.setTextureName(String.format("%s:%s", BWFConstants.MODID, id));
